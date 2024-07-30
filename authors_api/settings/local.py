95007b3118d60d1826e736651f223b91f2de95f6
Yaroslav Markviskyi <yaroslav.markivskyi.it.2022@lpnu.ua>
2024-07-30 13:17:08 +0300

    chore: setup basic django project

---
+++ authors_api/settings/local.py
@@ -0,0 +1,13 @@
+from .base import * #noqa
+from .base import env
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="X-RaTTGao4FEZoBTkD2whDHKRgwAMooVv0fpDUJP5o905-D66JE",
+)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+CSRF_TRUSTED_ORIGIN = ["http://localhost:8080"]
