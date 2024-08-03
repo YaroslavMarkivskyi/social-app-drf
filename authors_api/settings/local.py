3299280a14c967f4a7f45a169086b91258c9068e
Yaroslav Markviskyi <yaroslav.markivskyi.it.2022@lpnu.ua>
2024-08-03 12:06:30 +0300

    feat: configure celery, redis and flower

--- authors_api/settings/local.py
+++ authors_api/settings/local.py
@@ -11,3 +11,11 @@ SECRET_KEY = env(
 DEBUG = True
 
 CSRF_TRUSTED_ORIGIN = ["http://localhost:8080"]
+
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+DEFAULT_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "support@apiimperfect.site"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Authors Haven"
