47a6a3c65be20c1ffacf9455d088ff58a2bf18bb
Yaroslav Markviskyi <yaroslav.markivskyi.it.2022@lpnu.ua>
2024-08-07 20:20:45 +0300

    feat: profile view

--- core_apps/users/views.py
+++ core_apps/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class CustomUserDetailView(RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = IsAuthenticated
+
+    def get_objetc(self):
+        return self.request.user
+
+    def get_queryset(self):
+        return get_user_model().objects.none()
